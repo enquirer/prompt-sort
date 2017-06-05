3ea69650fb0cd27ad1ac7629318b2d64f3650987
jonschlinkert <jon.schlinkert@sellside.com>
2017-06-05 00:04:45 -0400

    update examples, docs

--- examples/prompt.js
+++ examples/prompt.js
@@ -1,31 +1,13 @@
-'use strict';
-
-var colors = require('ansi-colors');
 var Prompt = require('..');
 var prompt = new Prompt({
-  name: 'colors',
-  message: 'Sort colors from most to least favorite',
-  choices: [
-    'green',
-    'red',
-    'yellow'
-  ]
+  name: 'letters',
+  message: 'Sort letters',
+  choices: ['AAA', 'BBB', 'CCC']
 });
-
-var keys = prompt.choices.keys.slice();
-prompt.choices.options.format = function(line) {
-  var name = /\w+/.exec(line);
-  return name ? colors[name[0]](line) : line;
-};
 
 prompt.run()
   .then(function(answer) {
-    if (!answer) return;
-    console.log(colors.underline('Preferred colors'));
-    console.log(answer.map(function(color, i) {
-      var idx = keys.indexOf(color) + 1;
-      return (i + 1) + '. ' + colors[color](color) + ' (' + idx + ')';
-    }).join('\n'));
+    console.log({letters: answer});
   })
   .catch(function(err) {
     console.log(err);
